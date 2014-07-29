a8ae7b452456ff08b40b8b60e87a796b289d48b1
aweiland <aweiland@gmail.com>
2014-07-28 20:26:17 -0400

    Improved alert tag

--- grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
+++ grails-app/taglib/twitter/bootstrap/scaffolding/AlertsTagLib.groovy
@@ -1,13 +1,33 @@
 package twitter.bootstrap.scaffolding
 
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+
 class AlertsTagLib {
 
 	static namespace = "bootstrap"
 
+	static List<String> validTypes = [ "info", "success", "warning", "danger" ]
+	
+	/**
+	 * Create a Bootstrap alert
+	 * 
+	 * @attr type REQUIRED One of the valid Bootstrap alert types: "info", "success", "warning", "danger"
+	 * @attr dismissable If the alert is dismissable, ie there is a close button and it goes away.  Default is true;
+	 */
 	def alert = { attrs, body ->
-		out << '<div class="alert ' << attrs.class.tokenize().join(" ") << '">'
-		out << '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'
-		out << '<p>' << body() << '</p>'
+		def type = attrs.type ?: "info";
+		def dismissable = Boolean.valueOf(attrs.dismissable ?: true);
+		
+		if (!validTypes.contains(type)) {
+			throw new GrailsTagException("Invalid alert type")
+		}
+		
+		
+		out << '<div class="alert alert-' << type << '">'
+		if (dismissable) {
+			out << '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'
+		}
+		out << body()
 		out << '</div>'
 	}
 
